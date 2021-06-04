061d61dbb47b18b7a895c7d911a24258c6127ef5
doka <posmane5@gmail.com>
2021-06-04 10:45:35 +0900

    Add new valueable
    new value is added

---
+++ hello/ch4/ex4.1/ex4.1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var msg string = "hello world"
+
+	a = 20
+	msg = "good morning"
+	fmt.Println(msg, a)
+
+	b = 2030
+}
---
+++ hello/ch4/ex4.1/go.mod
@@ -0,0 +1,3 @@
+module ch4/ex4.1
+
+go 1.16
